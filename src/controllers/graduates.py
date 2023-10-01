58a93cddb40ab5f4e03a33471e3b71824e0d8bcf
guavacoderepo <guavacodeplus@gmail.com>
2023-10-01 23:50:26 +0100

    dummy date

--- src/controllers/graduates.py
+++ src/controllers/graduates.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, jsonify
+from src.modules.mongo import create_dummy
+from time import sleep
 
 
 graduates = Blueprint("gradustes", __name__, url_prefix="/api/v1/graduates")
@@ -7,6 +9,17 @@ graduates = Blueprint("gradustes", __name__, url_prefix="/api/v1/graduates")
 # generate dummy endpoint
 @graduates.route("/dummy", methods = ["GET"])
 def generate_dummy():
+    i = 0
+
+    while i<1000:
+
+        create_dummy()
+
+        i+=1
+
+        sleep(0.5)
+        
+        
     return "welcome dummy to dummy generator"
 
 
